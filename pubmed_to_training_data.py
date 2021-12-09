5c6e9edf0e3fbea78cacc2acaaab5e3af111fe26
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2021-12-09 15:34:43 +0100

    DEC2021

--- pubmed_to_training_data.py
+++ pubmed_to_training_data.py
@@ -1,4 +1,4 @@
-from pymed import PubMed
+from query import df_pubmed
 import pandas as pd
 import spacy
 import json
@@ -7,43 +7,7 @@ from tqdm import tqdm
 from spacy.tokens import DocBin
 
 
-def df_pubmed(query):
-    '''
-    Function to fetch PubMed data from a query.
-
-    '''
-    pubmed = PubMed(tool="PubMedSearcher", email="khalil.amardjia@gmail.com")
-
-    search_term = query
-
-    results = pubmed.query(search_term, max_results=100000)
-    articleList = []
-    articleInfo = []
-
-    for article in results:
-        articleDict = article.toDict()
-        articleList.append(articleDict)
-    try:
-        for article in articleList:
-            pubmedId = article['pubmed_id'].partition('\n')[0]
-            articleInfo.append({u'pubmed_id': pubmedId,
-                                u'title': article['title'],
-                                u'keywords': article['keywords'],
-                                u'journal': article['journal'],
-                                u'abstract': article['abstract'],
-                                u'conclusions': article['conclusions'],
-                                u'methods': article['methods'],
-                                u'results': article['results'],
-                                u'copyrights': article['copyrights'],
-                                u'doi': article['doi'],
-                                u'publication_date': article['publication_date'],
-                                u'authors': article['authors']})
-    except:
-        pass
-    articlesPD = pd.DataFrame.from_dict(articleInfo)
-    return articlesPD
-
-query = '"Arthroplasty, Replacement, Ankle"[Majr] AND ("Clinical Study" [Publication Type] OR "Comparative Study" [Publication Type] OR "Evaluation Study" [Publication Type] OR "Meta-Analysis" [Publication Type] OR "Multicenter Study" [Publication Type] OR "Systematic Review" [Publication Type])'
+query = 'Ortho AND ("Clinical Study" [Publication Type] OR "Comparative Study" [Publication Type] OR "Evaluation Study" [Publication Type] OR "Meta-Analysis" [Publication Type] OR "Multicenter Study" [Publication Type] OR "Systematic Review" [Publication Type])'
 df = df_pubmed(query= query)
 abstracts = df.abstract.dropna()
 abstracts = abstracts.drop_duplicates()
