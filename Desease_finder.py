be30e216fb91aa7e316a40cf6fe3a000077f3b40
KhalilAMARDJIA <47823346+KhalilAMARDJIA@users.noreply.github.com>
2022-05-01 15:50:36 +0200

    Add files via upload

---
+++ Desease_finder.py
@@ -0,0 +1,46 @@
+import spacy
+import pandas as pd
+from query import df_pubmed
+from plotly import express as px
+
+
+data = df_pubmed('(intraocular lens) AND ((y_10[Filter]) AND (fha[Filter]) AND (clinicaltrial[Filter] OR meta-analysis[Filter] OR randomizedcontrolledtrial[Filter] OR review[Filter] OR systematicreview[Filter])) ')
+text = ''
+
+for abstract in data.abstract:
+    text += abstract
+
+
+nlp = spacy.load("en_ner_bc5cdr_md")
+
+# Named entity recognition
+ 
+doc = nlp(text)
+
+ner_data = {
+    'entity':[],
+    'label':[]
+}
+
+for ent in doc.ents:
+    ner_data['entity'].append(ent.text)
+    ner_data['label'].append(ent.label_)
+
+ner_data = pd.DataFrame(ner_data)
+ner_table = pd.crosstab(ner_data['entity'], ner_data['label']).reset_index()
+
+display_n = 30
+ner_table = ner_table.sort_values('DISEASE', ascending= False).head(display_n).head(display_n).sort_values('DISEASE', ascending= True)
+
+fig = px.bar(
+    template = 'simple_white',
+    x = 'DISEASE',
+    y = 'entity', 
+    color_discrete_sequence=px.colors.diverging.curl,
+    data_frame=ner_table,
+    title=f'PubMed data extracted from abstracts'
+    )
+
+fig.update_traces(marker_line_color='black',marker_line_width=1)
+fig.update_layout(font_family="Courier New")
+fig.show()
